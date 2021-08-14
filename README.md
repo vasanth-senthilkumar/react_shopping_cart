afe01a4a84f850f3690ed19938387748d6720c39
vasanth-senthilkumar <vasanthfourbright@gmail.com>
2021-08-14 09:53:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create Shopping cart
---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+      <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All Rights are received
+      </footer>
+      </div>
+  );
+}
+
+export default App;
